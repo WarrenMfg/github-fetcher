bb5fc8cf2ab174f56907517b73701bd8ae519575
WarrenMfg <warrenmfg@icloud.com>
2020-01-18 17:25:29 -0500

    updated config for GitHub API key

--- helpers/github.js
+++ helpers/github.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const reqPromise = require('request-promise');
-const config = require('../config.js');
+// const config = require('../config.js');
 
 let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
@@ -12,7 +12,7 @@ let getReposByUsername = (username) => {
     url: `https://api.github.com/users/${username}/repos?type=owner&sort=pushed&direction=desc`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${config}`
     },
     json: true
   };
