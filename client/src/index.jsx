0a3f643ee0afc3f9ce58ba86800a0c01a5a265f6
WarrenMfg <warrenmfg@icloud.com>
2020-01-18 18:20:21 -0500

    troubleshooting GET and POST requests

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
     $.ajax({
-      url: '/repos',
+      url: 'https://damp-beach-94644.herokuapp.com/repos',
       success: this.addReposToState,
       error: function(err) {
         console.log('error at index.jsx search ajax', err);
@@ -28,7 +28,7 @@ class App extends React.Component {
     // TODO
     $.ajax({
       method: 'POST',
-      url: '/repos',
+      url: 'https://damp-beach-94644.herokuapp.com/repos',
       data: JSON.stringify({username: username}),
       contentType: 'application/json',
       success: this.addReposToState,
