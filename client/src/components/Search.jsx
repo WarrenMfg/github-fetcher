ed574f145e92c7cbb73d062bead830cda28804c7
WarrenMfg <warrenmfg@icloud.com>
2020-01-18 22:18:58 -0500

    merged Search.jsx: onChange and handleKeyDown

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,28 +6,33 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
-    this.onChange = this.onChange.bind(this);
+    // this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
-  onChange (e) {
-    this.setState({
-      term: e.target.value
-    });
-  }
+  // onChange (e) {
+  //   this.setState({
+  //     term: e.target.value
+  //   });
+  // }
 
   search() {
     this.props.onSearch(this.state.term);
     this.setState({term: ''});
   }
 
-  handleKeyUp(e) {
+  handleKeyDown(e) {
     let enter = (e.keyCode ? e.keyCode : e.which);
     if (enter === 13) {
       this.props.onSearch(this.state.term);
       this.setState({term: ''});
+      return;
     }
+
+    this.setState({
+      term: e.target.value
+    });
   }
 
   render() {
@@ -35,7 +40,7 @@ class Search extends React.Component {
     <div className="Search">
       <h2>Add more repos!</h2>
       <div>Enter a GitHub username:
-        <input value={this.state.term} onChange={this.onChange} onKeyUp={this.handleKeyUp}/>
+        <input value={this.state.term} onKeyDown={this.handleKeyDown}/>
         <button onClick={this.search}>Add Repos</button>
       </div>
     </div>);
