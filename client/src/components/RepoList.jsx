a25eab07f3d9db9297fec666242b54d7d6a0356d
WarrenMfg <warrenmfg@icloud.com>
2020-01-18 21:35:43 -0500

    added changes from local to master

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Repo from './Repo.jsx';
+import {CSSTransitionGroup} from 'react-transition-group';
 
 class RepoList extends React.Component {
   constructor(props) {
@@ -10,9 +11,14 @@ class RepoList extends React.Component {
     return (
       <div className="RepoList">
         <h3>Top 25 most recently pushed repos sorted by stargazer count</h3>
-        {this.props.repos.map(repo => <Repo repo={repo} key={repo.repoId}/>)}
+        <CSSTransitionGroup
+          transitionName="slideIn"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={250}
+        >
+          {this.props.repos.map(repo => <Repo repo={repo} key={repo.repoId}/>)}
+        </CSSTransitionGroup>
       </div>
-
     );
   }
 }
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,6 +8,7 @@ class Search extends React.Component {
     }
     this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   onChange (e) {
@@ -21,12 +22,19 @@ class Search extends React.Component {
     this.setState({term: ''});
   }
 
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.props.onSearch(this.state.term);
+      this.setState({term: ''});
+    }
+  }
+
   render() {
     return (
     <div className="Search">
       <h2>Add more repos!</h2>
       <div>Enter a GitHub username:
-        <input value={this.state.term} onChange={this.onChange}/>
+        <input value={this.state.term} onChange={this.onChange} onKeyPress={this.handleKeyPress}/>
         <button onClick={this.search}>Add Repos</button>
       </div>
     </div>);
