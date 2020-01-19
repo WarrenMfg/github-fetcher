67b6ea6bd65ac7f45e5410d22da1da3adcd76f94
WarrenMfg <warrenmfg@icloud.com>
2020-01-18 22:35:19 -0500

    added variations and logical OR for enter

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -6,33 +6,30 @@ class Search extends React.Component {
     this.state = {
       term: ''
     }
-    // this.onChange = this.onChange.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.search = this.search.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
-  // onChange (e) {
-  //   this.setState({
-  //     term: e.target.value
-  //   });
-  // }
+  onChange (e) {
+    this.setState({
+      term: e.target.value
+    });
+  }
 
   search() {
     this.props.onSearch(this.state.term);
     this.setState({term: ''});
   }
 
-  handleKeyDown(e) {
+  handleKeyPress(e) {
     let enter = (e.keyCode ? e.keyCode : e.which);
-    if (enter === 13) {
+    console.log(event)
+    if (enter === 13 || e.key === 'Enter') {
       this.props.onSearch(this.state.term);
       this.setState({term: ''});
       return;
     }
-
-    this.setState({
-      term: e.target.value
-    });
   }
 
   render() {
@@ -40,7 +37,7 @@ class Search extends React.Component {
     <div className="Search">
       <h2>Add more repos!</h2>
       <div>Enter a GitHub username:
-        <input value={this.state.term} onKeyDown={this.handleKeyDown}/>
+        <input value={this.state.term} onChange={this.onChange} onKeyPress={this.handleKeyPress}/>
         <button onClick={this.search}>Add Repos</button>
       </div>
     </div>);
